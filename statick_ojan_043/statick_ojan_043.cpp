0d11bc5866edf162f4da9465e0a2cb13d23fb1c6
zanthegreat18 <sulistiyofauzan27@gmail.com>
2023-05-17 15:41:08 +0700

    commit 7

--- statick_ojan_043/statick_ojan_043.cpp
+++ statick_ojan_043/statick_ojan_043.cpp
@@ -26,3 +26,18 @@ void mahasiswa::printALL() {
 	cout << "Nim = " << nim << endl;
 	cout << endl;
 }
+
+int main() {
+	mahasiswa mhs1("Lia Kurnia");
+	mahasiswa mhs2("Asroni");
+	mhs2.nim = 10;
+	mahasiswa mhs3("Andi Kurniawan");
+	mhs3.nim = 20;
+	mahasiswa mhs4("Joko Purbo");
+
+	mhs1.printALL();
+	mhs2.printALL();
+	mhs3.printALL();
+	mhs4.printALL();
+	return 0;
+}
